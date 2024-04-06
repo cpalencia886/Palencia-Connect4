c4bc0882152defecb181ef149859b83cdfe9e003
cpalencia886 <c.palencia886@mybvc.ca>
2024-04-05 23:59:58 -0600

    restart code - add interface

--- Program.cs
+++ Program.cs
@@ -4,39 +4,17 @@
 // Course: SODV1202:Introduction to Object Oriented Programming-24JANMNTR1 
 
 using System;
-using System.Runtime.CompilerServices;
 
-//Class 2: ProjectConnectFour
-// Connect four game flow
+// Interface: MyBoard 
+// To define methods for game info display
 
-public class ProjectConnectFour {
-    private Board gameBoard; // to hold my gameboard
-    private Player playerSwitch; //to swicth currentr player
-    private Player player1; 
-    private Player player2;
+public interface MyBoard
+{
+    void DisplayWelcome ();
+    void DisplayPlayerTurn(char symbol);
+    void DisplayBoard(Board gameBoard, Player playerSwitch);
+    void PromptForColumn();
+    void DisplayWinner(char symbol);
+    int PromptForRestart();
 
-//to set 2 players
-public class ProjectConnectFive (Player player1, Player player2 ){
-
-    this.player1 = player1;
-    this.player2 = player2;
-    this.playerSwitch = player1;
-    this.gameBoard = new Board();
-    
-}
-}
-//Class 1: MaIN Game
-class MainGame
-{ //Changed name Program to MainGame
-
-    static void Main(string[] args)
-    {
-
-        Player player1 = new Player('X');
-        Player player2 = new player1('0');
-        ProjectConnectFour game = new ProjectConnectFour(player1, player2);
-
-        game.letsStart()
-
-    }
 }
